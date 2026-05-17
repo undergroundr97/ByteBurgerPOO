42fda59b601cdf6b34b8bb91c7b111a5361a38e7
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-16 23:56:32 -0300

    feat(menu): adiciona menu e submenu com metodos estaticos

---
+++ src/main/java/org/Menu/Menu.java
@@ -0,0 +1,22 @@
+package org.Menu;
+
+public class Menu {
+    public static void exibirMenu(){
+        System.out.println("1 - Novo Pedido");
+        System.out.println("2 - Consultar Cardapio por " +
+                "Categoria");
+        System.out.println("3 - Calcular Troco");
+        System.out.println("4 - Sorteio do Dia");
+        System.out.println("0 - Encerrar o Sistema");
+        System.out.println("Digite opção:");
+    }
+
+    public static void subMenu(){
+        System.out.println("-------------------");
+        System.out.println("1 - Lanches");
+        System.out.println("2 - Acompanhmentos");
+        System.out.println("3 - Bebidas");
+        System.out.println("0 - Voltar");
+        System.out.println("Digite opção:");
+    }
+}
