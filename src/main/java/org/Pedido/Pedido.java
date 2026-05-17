37472859e21b5071a6e6a047b10f04b53f126a7a
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-17 00:48:22 -0300

    feat(pedido): adiciona total do pedido, adiciona resumo do pedido com o valor total

--- src/main/java/org/Pedido/Pedido.java
+++ src/main/java/org/Pedido/Pedido.java
@@ -5,19 +5,27 @@ import java.util.ArrayList;
 
 public class Pedido {
     ArrayList<ItemCardapio> itensSelecionados;
-    String nome;
-
+    public String nome;
+    private Double totalPedido = 0.0;
     public Pedido(String nome, ArrayList<ItemCardapio> itensSelecionados){
         this.nome = nome;
         this.itensSelecionados = itensSelecionados;
     }
+    public Double getTotalPedido(){
+        return totalPedido;
+    }
 
     public void mostrarPedido(){
-        System.out.println("---- RESUMO PEDIDO----");
-        System.out.println("Nome: " + nome);
+        System.out.println("------- RESUMO PEDIDO -------");
+        System.out.println(" -> Nome: " + nome.toUpperCase());
         for (ItemCardapio itensSelecionado : itensSelecionados) {
-            System.out.println(" -> " + itensSelecionado.nome + " R$" + itensSelecionado.preco);
+            System.out.println(" -> " + itensSelecionado.getNome() + " - R$" + String.format("%.2f",
+                    itensSelecionado.getPreco()) );
+            totalPedido += itensSelecionado.getPreco();
+
         }
+        System.out.println("----------- TOTAL ------------ ");
+        System.out.println(" == Total do pedido: R$"+String.format("%.2f", getTotalPedido())+  " ");
     }
 
 }
