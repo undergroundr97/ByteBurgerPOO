aecfafffae20f5b7bac7ca3f1b978baba01d641e
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-17 01:30:55 -0300

    feat(inputvalidator): adiciona verificao s/n

--- src/main/java/org/InputValidor/InputValidator.java
+++ src/main/java/org/InputValidor/InputValidator.java
@@ -3,11 +3,20 @@ package org.InputValidor;
 import java.util.Scanner;
 
 public class InputValidator {
+    static Scanner scanner = new Scanner(System.in);
     public static boolean intValidator(Scanner scanner){
        while(!scanner.hasNextInt()){
            System.out.println("Input invalido");
            scanner.next();
        }
        return true;
+    }
+
+    public static boolean yesNoValidator(String yesNo){
+        while(!yesNo.equalsIgnoreCase("s") && !yesNo.equalsIgnoreCase("n")){
+            System.out.println("Input invalido, digite S/N");
+            yesNo = scanner.nextLine();
+        }
+        return true;
     }
 }
---
+++ src/main/java/org/SwitchUserInput/SwitchInput.java
@@ -0,0 +1,4 @@
+package org.SwitchUserInput;
+
+public class SwitchInput {
+}
