28414c4933c8475d22e1dbc8e4116a85a454921c
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-17 00:49:43 -0300

    fix(itemcardapio): conserta forma para chamar os valores do itempedido

--- src/main/java/org/Pedido/ItemCardapio.java
+++ src/main/java/org/Pedido/ItemCardapio.java
@@ -1,12 +1,19 @@
 package org.Pedido;
 
 public class ItemCardapio {
-    String nome;
-    Double preco;
+    private String nome;
+    private Double preco;
 
     public ItemCardapio(String nome, Double preco){
         this.nome = nome;
         this.preco = preco;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
 }
