8062c29e2e152d6f0f07a940a41b43ff3a0f9bda
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-18 09:44:23 -0300

    feat(inputvalidator): adiciona double validator

--- src/main/java/org/InputValidor/InputValidator.java
+++ src/main/java/org/InputValidor/InputValidator.java
@@ -12,6 +12,14 @@ public class InputValidator {
        return true;
     }
 
+    public static boolean doubleValidator(Scanner scanner){
+        while(!scanner.hasNextDouble()){
+            System.out.println("Input invalido");
+            scanner.next();
+        }
+        return true;
+    }
+
     public static String yesNoValidator(String yesNo){
         while(!yesNo.equalsIgnoreCase("s") && !yesNo.equalsIgnoreCase("n")){
             System.out.println("Input invalido, digite S/N");
