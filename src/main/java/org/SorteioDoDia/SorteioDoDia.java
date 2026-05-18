5cde8809807c8e8dc0d5e4992d2f6e80c7bf2818
luiztestonidev <luiztestoni5@gmail.com>
2026-05-18 11:42:57 -0300

    fix: corrige bugs de desconto

--- src/main/java/org/SorteioDoDia/SorteioDoDia.java
+++ src/main/java/org/SorteioDoDia/SorteioDoDia.java
@@ -6,19 +6,20 @@ import org.Pedido.ItemCardapio;
 public class SorteioDoDia {
 
     static int itemCardapioSorteado = (int) (Math.random() * 6);
+    static ItemCardapio itemSorteado = Cardapio.getItemCardapio(itemCardapioSorteado);
+    static double precoDesconto = itemSorteado.getPreco() * 0.80;
 
     public static void sorteioDoDia() {
 
-        ItemCardapio itemSorteado = Cardapio.getItemCardapio(itemCardapioSorteado);
-
         double precoOriginal = itemSorteado.getPreco();
-
-        double precoDesconto = precoOriginal * 0.80;
 
         System.out.println("\n------ SORTEIO DO DIA ------");
         System.out.println("O item sorteado do dia: " + itemSorteado.getNome() + " está com 20% de desconto!");
         System.out.printf("Preço original: R$ %.2f%n", precoOriginal);
         System.out.printf("Preço com desconto: R$ %.2f%n", precoDesconto);
+    }
+
+    public static void descontoSorteado() {
         itemSorteado.setPreco(precoDesconto);
     }
 }
