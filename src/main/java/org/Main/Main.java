0410ece2699cec17d3aacaa93ed6c38837237680
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2026-05-18 09:50:25 -0300

    fix(main): chama corretamente byteburger class

--- src/main/java/org/Main/Main.java
+++ src/main/java/org/Main/Main.java
@@ -1,15 +1,10 @@
 package org.Main;
 
-import org.InputValidor.InputValidator;
+import org.ByteBurger.ByteBurger;
 
-import java.util.Scanner;
 
 public class Main {
     static void main() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite um numero");
-        InputValidator.intValidator(scanner);
-        int variavelTeste = scanner.nextInt();
-        System.out.println(variavelTeste);
-    }
+        ByteBurger.byteBurgerStart();
+     }
 }
